89211e6229a15559faf930dc20b62784c314446e
Saugat Poudel <njsaugat@gmail.com>
2023-01-03 17:51:52 +0545

    infer file added

--- infer.ts
+++ infer.ts
@@ -2,3 +2,4 @@ function increaseSalary(salary: number): number {
   let increasedSalary = salary * 1.1; //no need to define increasedSalary to number--> infered as number
   return increasedSalary;
 }
+export {}; // to make it as object
